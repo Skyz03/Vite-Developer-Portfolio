471b6385ba7c377ba93cce9056227e103fdac120
Asia Ashraf <asiaashraf7272@gmail.com>
2025-10-06 17:56:40 +0500

    fix error in contact section

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 import Hero from './components/Hero';
+import Nav from "./components/Nav"
 import Skills from './components/Skills';
 import Projects from './components/Projects';
+import Contact from './components/Contact';
 
 function App() {
   return (
